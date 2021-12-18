635255d91f5ea23cdb4d02afb6b548cdc43a2833
Chris Crain <chrisac309@gmail.com>
2021-12-18 18:13:46 +0000

    Successfully get current wifi speeds

--- app.py
+++ app.py
@@ -4,8 +4,37 @@
 #-----------------------------------------------------------------------------------------
 
 from flask import Flask
+from flask.templating import render_template
+import speedtest
+
+st = speedtest.Speedtest()
+bytesInMb = 1048576
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return app.send_static_file("index.html")
+    upload = st.upload()/bytesInMb
+    download = st.download()/bytesInMb
+    servernames =[]
+    st.get_servers(servernames)
+    return render_template("index.html", uploadSpeed=upload, downloadSpeed=download, ping=st.results.ping)
+
+# There are negative implications when running this in a docker container.  The next step will be to determine the best way to run this
+# import os
+
+# # scan available Wifi networks
+# os.system('netsh wlan show networks')
+
+# # input Wifi name
+# name_of_router = input('Enter Name/SSID of the Wifi Network you wish to connect to: ')
+
+# # connect to the given wifi network
+# os.system(f'''cmd /c "netsh wlan connect name={name_of_router}"''')
+
+# print("If you're not yet connected, try connecting to a previously connected SSID again!")
+
+# upload = st.upload()/bytesInMb
+# download = st.download()/bytesInMb
+# servernames =[]
+# st.get_servers(servernames)
